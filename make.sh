70e694eaab4ca90030b5c3e6c45c933afc62a709
Manlio Perillo <manlio.perillo@gmail.com>
2023-03-08 14:54:41 +0100

    make.sh: remove tabs
    Remove tabs in the make.sh script.  They where inadvertently copied from
    an early draft where a Makefile was used.

--- make.sh
+++ make.sh
@@ -5,20 +5,20 @@ docker_name=zig-tty-test-data
 docker_cmd=${1:-""}
 
 case "$docker_cmd" in
-	build)
-	    # Ensure the docker user has the same UID as the current user.
-	    docker_uid="$(id -u)"
+    build)
+        # Ensure the docker user has the same UID as the current user.
+        docker_uid="$(id -u)"
 
-	    sudo docker buildx build -t "$docker_name" --no-cache \
-	        --build-arg uid="$docker_uid" --progress tty .
-		;;
-	run)
+        sudo docker buildx build -t "$docker_name" --no-cache \
+            --build-arg uid="$docker_uid" --progress tty .
+        ;;
+    run)
         sudo docker run --rm --name "$docker_name" -it \
             --mount type=bind,source="$(PWD)"/src,target=/home/zig/src,readonly \
             --mount type=bind,source="$(PWD)"/data,target=/home/zig/data \
             "$docker_name":latest
-		;;
-	run-sh)
+        ;;
+    run-sh)
         sudo docker run --rm --name "$docker_name" -it --entrypoint sh \
             "$docker_name":latest
         ;;
@@ -33,5 +33,4 @@ case "$docker_cmd" in
        printf "$0: error: invalid command '%s'.\n" "$docker_cmd"
        exit 1
        ;;
-
 esac
